7dd30de1b661eec499dfa48d5f90d2366ed2b165
Han Zin <hanzintun1411@gmail.com>
2022-12-25 21:07:08 +0630

    coffee-shop-finished

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 const countElement = document.querySelector(".count");
 let count = 0;
 let customerCount = [];
+
 // cannot write with const since it cannot be reassigned
 const titleElement = document.querySelector("h1");
 let greeting = "Welcome";
 let customerName = "Hazel";
 
-countElement.innerText = count;
+const totalCount = document.querySelector("h2");
+
+countElement.textContent = count;
 
 function increase() {
-  count = count + 1;
-  countElement.innerText = count;
+  count += 1;
+  // count = count + 1;
+  countElement.textContent = count;
 }
 function decrease() {
-  count = count - 1;
-  countElement.innerText = count;
+  if (count > 0) {
+    //count = count - 1;
+    count -= 1;
+  }
+  countElement.textContent = count;
 }
 
 function save() {
   //console.log("saved", count);
   customerCount.push(count);
-  console.log(customerCount);
+  totalCount.textContent = "Daily Saved Counts are" + " " + customerCount;
 }
 
-function greet(){
-titleElement.innerText = greeting + " " + customerName;
+function greet() {
+  titleElement.textContent = greeting + " " + customerName;
+}
+
+function reset() {
+  count = 0;
+
+  countElement.textContent = count;
 }
